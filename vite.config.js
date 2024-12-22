35758333c54d06a4cc2585bfeaa62ebced9625f1
Alina <151466607+volohaalina@users.noreply.github.com>
2024-12-23 01:04:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  // Handle React Router fallback for non-root URLs
-  server: {
-    historyApiFallback: true,
-  },
+ base: "
+https://volohaalina.github.io/cv4",
 });
